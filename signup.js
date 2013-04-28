141605074f1da275a2b2aeb801c823ae642a5b27
calvinstudebaker <calstud@stanford.edu>
2013-04-28 03:53:48 -0700

    time table added. looks pretty beautiful

--- signup.js
+++ signup.js
@@ -2,13 +2,43 @@ function submitInfo(){
 	alert("clicked");
 };
 
+function createTimeTable(){
+	$.getJSON("times.json", function(data){
+		var row = -1;
+		$.each(data.times, function(index, element){
+			if(index % 3 == 0){
+				row++;
+				$(".timeTable").append("<tr class='row"+row+"'></tr>");
+			}
+			$("<td class='hook'>"+element+"</td>").appendTo(".row"+row).click(function(){
+				if($(this).hasClass("selected")){
+					$(this).removeClass("selected");
+				}else{
+					$(this).addClass("selected");
+				}
+			});
+		});
+	});
+};
+
+function showTimeTable(){
+	if($("#roomNumber").val().length > 0 && $("#name").val().length > 0)
+		$(".times").show("slow");
+};
+
 $(document).ready(function(){
-	$(".content").hide();
-	$(".content").show("slow");
+	$(".left").hide();
+	$(".times").hide();
 
 	$("input[type=text]").focusin(function() {
 		$(this).addClass("highlighted");
 	}).focusout(function() {
 		$(this).removeClass("highlighted").removeClass("error");
 	});
+
+	createTimeTable();
+
+	$(".left").show("slow");
+	$("#roomNumber").keyup(showTimeTable);
+	$("#name").keyup(showTimeTable);
 });
