b3a2453aa58a63d964593099fb32bb7c84acb046
calvinstudebaker <calstud@stanford.edu>
2013-06-09 18:05:49 -0700

    working on scheduler

--- js/createSchedule.js
+++ js/createSchedule.js
@@ -9,14 +9,58 @@ exports.createSchedule = function(){
 function startCreateSchedule(requests){
   var timeMap = buildTimeMap(requests);
   console.log(timeMap);
+  buildSchedule(timeMap);
 };
 
 function buildSchedule(timeMap){
-	for(var time in timeMap){
-		var requests = timeMap[time];
+  var sortedTimes = sortTimeMap(timeMap);
+  var schedule = new Object;
+	for(var elem in sortedTimes){
+		if(sortedTimes[elem].requests.length == 1){
+      schedule[sortedTimes[elem].time] = sortedTimes[elem].requests[0];
+      removeRequest(sortedTimes, sortedTimes[elem].requests[0]);
+    }
 	}
 };
 
+function removeRequest(sortedTimes, request){
+  for(var elem in sortedTimes){
+    var requests = sortedTimes[elem].requests;
+    for(var i = 0; i < requests.length; i++){
+      var curr = requests[i];
+      if(isEqual(curr, request)){
+        requests.splice(i, 1);
+        i--;
+      }
+    }
+  }
+};
+
+function isEqual(request1, request2){
+  return (request1.firstName == request2.firstName && request1.lastName == request2.lastName && request1.roomNumber == request2.roomNumber);
+};
+
+//Insertion sorts the elements in the timeMap by the number of requests for a given time
+function sortTimeMap(timeMap){
+  var sortedTimes = new Array();
+  for(var time in timeMap){
+    var elem = new Object();
+    elem.time = time;
+    elem.requests = timeMap[time];
+    var added = false;
+    for(var index in sortedTimes){
+      if(sortedTimes[index].requests.length >= elem.requests.length){
+        sortedTimes.splice(index, 0, elem);
+        added = true;
+        break;
+      }
+    }
+    if(!added){
+      sortedTimes.push(elem);
+    }
+  }
+  return sortedTimes;
+}
 
 function buildTimeMap(requests){
   timeMap = new Object();
@@ -60,3 +104,9 @@ function getAllRequests(callback){
   });
 };
 
+function logSortedTimes(sortedTimes){
+  for(var index in sortedTimes){
+    var elem = sortedTimes[index];
+    console.log(elem.time + ": " + elem.requests);
+  }
+};
